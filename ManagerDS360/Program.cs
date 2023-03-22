4b01d4fbd1c153acf7509ae512131554f1e79cb2
Максим Богащенко <sergeyivanov85@yandex.ru>
2023-03-22 19:12:17 +0300

    Косметические изменения кода.

--- ManagerDS360/Program.cs
+++ ManagerDS360/Program.cs
@@ -17,6 +17,8 @@ namespace ManagerDS360
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //тестовая строка
         }
     }
 }

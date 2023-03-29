b3b14cab5783e3c8e57e16e380bd6288d1f37778
Владимир Александрович Кирдяшкин <kva@vast.su>
2023-03-29 18:25:04 +0300

    Окно назначение генератора по умолчанию

---
+++ ManagerDS360/Окно назначение генератора по умолчанию.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagerDS360
+{
+    public partial class Окно_назначение_генератора_по_умолчанию : Form
+    {
+        public Окно_назначение_генератора_по_умолчанию()
+        {
+            InitializeComponent();
+        }
+    }
+}

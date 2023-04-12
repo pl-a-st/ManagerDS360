22a7848e2ba5e87043fbfbfefe773c381262b594
Andrey Markov <arherower@outlook.com>
2023-04-12 19:07:15 +0300

    DAO

--- ManagerDS360/Program.cs
+++ ManagerDS360/Program.cs
@@ -30,7 +30,8 @@ namespace ManagerDS360
 
         public DAO()
         {
-             
+            RouteFileInfoList.AddRange(new DirectoryInfo("W:\\8.Технический отдел\\Общая\\Группа C#\\Папка пользователя").GetFiles());
+            
         }
 
     }

ca9e33ed42b1f65b20dfc69d6693c54267b5a5bd
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2023-04-21 18:27:23 +0300

    Добавил конструкторы для DS360Setting

--- LibDevicesManager/DS360.cs
+++ LibDevicesManager/DS360.cs
@@ -22,21 +22,47 @@ namespace LibDevicesManager
     {
         Sine, Square, WhiteNoise, PinkNoise, TwoTone
     }
+    public enum ToneBFunctionType
+    {
+        Sine, Square
+    }
     public class DS360Setting
     {
-        public static string ComPortDefault;
-        public string ComPort { get; set; }
+        public static string ComPortDefaultName {get; set;}
+        public string ComPortName { get; set; }
         public DeviceModel Model { get { return DeviceModel.DS360; } }
         public DeviceType DeviceType { get { return DeviceType.Generator; } }
-        public FunctionType FunctionType { get; set; }
         public string SerialNumber { get; }
+        public FunctionType FunctionType { get; set; }
         public double Frequency { get; set; }
         public double AmplitudeRMS { get; set; }
         public double Offset { get; set; }
 
+        //
+        private static string comPortDefault;
 
 
-
+        //Конструкторы для SingleSignal
         public DS360Setting() { }
+        public DS360Setting(double amplitudeRMS, double frequency) { }
+        public DS360Setting(string portName, double amplitudeRMS, double frequency) { }
+        public DS360Setting(FunctionType functionType, double amplitudeRMS, double frequency) { }
+        public DS360Setting(string portName, FunctionType functionType, double amplitudeRMS, double frequency) { }
+        public DS360Setting(double amplitudeRMS, double  frequency, double offset) { }
+        public DS360Setting(string portName, double amplitudeRMS, double frequency, double offset) { }
+        public DS360Setting(FunctionType functionType, double amplitudeRMS, double frequency, double offset) { }
+        public DS360Setting(string portName, FunctionType functionType, double amplitudeRMS, double frequency, double offset) { }
+        //Конструкторы для TwoTone
+        public DS360Setting(double frequency_A, double amplitudeRMS_A, double frequency_B, double amplitudeRMS_B) { }
+        public DS360Setting(double frequency_A, double amplitudeRMS_A, double frequency_B, double amplitudeRMS_B, ToneBFunctionType functionType) { }
+        public DS360Setting(string portName, double frequency_A, double amplitudeRMS_A, double frequency_B, double amplitudeRMS_B) { }
+        public DS360Setting(string portName, double frequency_A, double amplitudeRMS_A, double frequency_B, double amplitudeRMS_B, ToneBFunctionType functionType) { }
+
+        public void SetComPortAsDefault (string portName) { }
+        public static List<string> GetComPortList()
+        {
+            List<string> portsNamesList = ComPort.PortsNamesList;
+            return portsNamesList;
+        }
     }
 }

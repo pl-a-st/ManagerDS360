39af0a99e685bdb5ae95d980aeb83124c37d3bcd
Максим Богащенко <pl_a_st@live.ru>
2023-06-10 22:06:43 +0300

    Добавил DS360SettingVibroSigParam наследник DS360Setting

---
+++ LibControls/ProgramClasses.cs
@@ -0,0 +1,19 @@
+﻿using LibDevicesManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibroMath;
+
+namespace LibControls
+{
+    [Serializable]
+    public class DS360SettingVibroSigParam : DS360Setting
+    {
+        public VibroParametr VibroParametr;
+        public SignalParametrType SignalParametrTone1;
+        public SignalParametrType SignalParametrTone2;
+        public Sensitivity Sensitivity;
+    }
+}

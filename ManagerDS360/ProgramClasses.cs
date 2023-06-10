3c176a5621608205ab362f0b2739e26bf4548b86
Максим Богащенко <pl_a_st@live.ru>
2023-06-10 22:09:52 +0300

    Добавил файл ProgramClasses в LibControls

--- ManagerDS360/ProgramClasses.cs
+++ ManagerDS360/ProgramClasses.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using LibDevicesManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VibroMath;
 
 namespace ManagerDS360
 {
-    internal class ProgramClasses
-    {
-    }
+    
 }

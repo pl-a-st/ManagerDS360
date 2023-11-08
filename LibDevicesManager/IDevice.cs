a72dad55fdd9151a47828fca24fcc5358e7da5bf
Верин Сергей <vsg@vast.su>
2023-11-08 18:32:11 +0300

    add IDevice

---
+++ LibDevicesManager/IDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDevicesManager
+{
+    interface IDevice
+    {
+        Result Send();
+        string Address
+        {
+            get;
+            set;
+        }
+    }
+    
+}

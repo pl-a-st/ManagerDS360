6d19b11075da28f549ae14a9ec69248fe7820025
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2023-12-05 18:57:27 +0300

    RFCT: перенёс девайс-енамы в интерфейсы

--- LibDevicesManager/Interfaces.cs
+++ LibDevicesManager/Interfaces.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace LibDevicesManager
 {
+    public enum Result
+    {
+        Success, Failure, Exception, ParamError, AcsessError, Canceled
+    }
+    public enum DeviceType
+    {
+        Generator,
+        Multimeter
+    }
+    public enum DeviceModel
+    {
+        DS360,
+        DS360Emulator,
+        Agilent33220A,
+        Agilent3458A,
+        Agilent34401A
+    }
     interface IDevice
     {
         DeviceType DeviceType { get; }
@@ -15,7 +32,7 @@ namespace LibDevicesManager
     }
     interface IGenerator : IDevice
     {
-        
+
     }
     interface IMultimeter : IDevice
     {

a4059fd070772b8e8ad3364abfaa948b7d7060d4
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2023-12-06 18:37:46 +0300

    ADD: on Multimeter.class

--- LibDevicesManager/Multimeter.cs
+++ LibDevicesManager/Multimeter.cs
@@ -15,8 +15,18 @@ namespace LibDevicesManager
         public DeviceType DeviceType { get { return DeviceType.Multimeter; } }
 
         public DeviceModel DeviceModel { get { return deviceModel; } set { deviceModel = value; } }
-        private DeviceModel deviceModel = DeviceModel.Unknown;
 
+        private DeviceModel deviceModel = DeviceModel.Unknown;
+        public Multimeter() { }
+        public Result SendSetting()
+        {
+            return Result.Failure;
+        }
+        public double Measure()
+        {
+            double value = 0;
+            return value;
+        }
         public string Receive()
         {
             if (deviceModel == DeviceModel.Agilent3458A)

12e0f68e0643142f87ee51d68188694f9d45192c
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2023-12-15 19:34:44 +0300

    ADD: поля в Generator.cs

--- LibDevicesManager/Generator.cs
+++ LibDevicesManager/Generator.cs
@@ -9,21 +9,34 @@ namespace LibDevicesManager
     [Serializable]
     public class Generator : IGenerator
     {
-        public DeviceType DeviceType {get { return DeviceType.Generator;}}
+        public DeviceType DeviceType { get { return DeviceType.Generator; } }
 
         public GeneratorModel GeneratorModel { get { return generatorModel; } set { generatorModel = value; } }
-        private GeneratorModel generatorModel = GeneratorModel.Unknown;
+        public FunctionType FunctionType { get { return FunctionType.Sine; } }
+        public double AmplitudeRMS { get { return amplitudeRMS; } set { amplitudeRMS = value; } }
+        public double Frequency { get { return frequency; } set { frequency = value; } }
+        public double Offset { get { return offset; } set { offset = value; } }
+        public OutputImpedance OutputImpedance { get { return OutputImpedance.HiZ; } }
         static public string Address { get { return address; } set { address = value; } }
-        static private string address = string.Empty;
 
+        private GeneratorModel generatorModel = GeneratorModel.Unknown;
+        static private string address = string.Empty;
+        private double amplitudeRMS = 0.1;
+        private double frequency = 160;
+        private double offset = 0;
+        public Generator() { }
+        public Result SendSetting()
+        {
+            return Result.Failure;
+        }
         public string Receive()
         {
-            throw new NotImplementedException();
+            return "ЗАПОЛНИТЬ";
         }
 
         public Result Send(string command)
         {
-            throw new NotImplementedException();
+            return Result.Failure;
         }
     }
 }
